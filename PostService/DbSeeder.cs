61593686174026232f7b6160e230a3eecb3142f7
Kozi <kaikozi150@gmail.com>
2025-06-04 16:05:59 +0200

    fix services

--- PostService/DbSeeder.cs
+++ PostService/DbSeeder.cs
@@ -10,59 +10,6 @@ namespace PostService
         {
             using var scope = services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-            // Upewnij się, że baza danych istnieje
-            await context.Database.EnsureCreatedAsync();
-
-            // Dodaj użytkownika, jeśli nie istnieje
-            if (!await context.Users.AnyAsync())
-            {
-                var user = new User
-                {
-                    Id = Guid.NewGuid(),
-                    Username = "admin",
-                    Email = "admin@threadia.pl",
-                    CreatedAt = DateTime.UtcNow
-                };
-
-                context.Users.Add(user);
-                await context.SaveChangesAsync();
-            }
-
-            // Dodaj tagi, jeśli nie istnieją
-            if (!await context.Tags.AnyAsync())
-            {
-                var tags = new[]
-                {
-                    new Tag { Id = Guid.NewGuid(), Name = "dotnet" },
-                    new Tag { Id = Guid.NewGuid(), Name = "csharp" },
-                    new Tag { Id = Guid.NewGuid(), Name = "threadia" }
-                };
-
-                context.Tags.AddRange(tags);
-                await context.SaveChangesAsync();
-            }
-
-            // Dodaj post, jeśli nie istnieje
-            if (!await context.Posts.AnyAsync())
-            {
-                var user = await context.Users.FirstOrDefaultAsync();
-                var tags = await context.Tags.ToListAsync();
-
-                var post = new Post
-                {
-                    Id = Guid.NewGuid(),
-                    Content = "Witaj w Threadia!",
-                    CreatedAt = DateTime.UtcNow,
-                    AuthorId = user.Id,
-                    Tags = tags
-                };
-
-                context.Posts.Add(post);
-                await context.SaveChangesAsync();
-            }
         }
     }
-
-    
 }
--- UserService/DbSeeder.cs
+++ UserService/DbSeeder.cs
@@ -28,41 +28,6 @@ namespace UserService
                 context.Users.Add(user);
                 await context.SaveChangesAsync();
             }
-
-            // Dodaj tagi, jeśli nie istnieją
-            if (!await context.Tags.AnyAsync())
-            {
-                var tags = new[]
-                {
-                    new Tag { Id = Guid.NewGuid(), Name = "dotnet" },
-                    new Tag { Id = Guid.NewGuid(), Name = "csharp" },
-                    new Tag { Id = Guid.NewGuid(), Name = "threadia" }
-                };
-
-                context.Tags.AddRange(tags);
-                await context.SaveChangesAsync();
-            }
-
-            // Dodaj post, jeśli nie istnieje
-            if (!await context.Posts.AnyAsync())
-            {
-                var user = await context.Users.FirstOrDefaultAsync();
-                var tags = await context.Tags.ToListAsync();
-
-                var post = new Post
-                {
-                    Id = Guid.NewGuid(),
-                    Content = "Witaj w Threadia!",
-                    CreatedAt = DateTime.UtcNow,
-                    AuthorId = user.Id,
-                    Tags = tags
-                };
-
-                context.Posts.Add(post);
-                await context.SaveChangesAsync();
-            }
         }
     }
-
-    
 }
